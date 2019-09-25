556756315ce211d7c49024ba1606f67ca07f1096
mdepp <mdepp@ualberta.ca>
2019-09-25 16:36:39 -0600

    Fix target pose estimate

--- src/follow_bot/scripts/target_position_estimator.py
+++ src/follow_bot/scripts/target_position_estimator.py
@@ -6,6 +6,7 @@ from sensor_msgs.msg import PointCloud2
 from sensor_msgs import point_cloud2
 import numpy as np
 import tf
+import ros_numpy
 
 
 class TargetPositionEstimator:
@@ -21,21 +22,28 @@ class TargetPositionEstimator:
         average = np.zeros((3,))
         num_points = 0
 
-        min_corner = np.array([-.2, -.3, -10])
-        max_corner = np.array([.2, .5, 1.2])
+        min_corner = np.array([-1, -10, 0])
+        max_corner = np.array([1, 0.2, 10])
 
-        for point in point_cloud2.read_points(pointcloud, skip_nans=True):
-            point = np.array(point[1:])  # type: np.ndarray
-            if np.all((point > min_corner) & (point < max_corner)):
-                average += point
-                num_points += 1
-        if num_points == 0:
+        self.min_x = rospy.get_param("~min_x", -0.2)
+        self.min_y = rospy.get_param("~min_y", -0.3)
+
+        self.max_x = rospy.get_param("~max_x", 0.2)
+        self.max_y = rospy.get_param("~max_y", 0.5)
+        self.max_z = rospy.get_param("~max_z", 1.2)
+
+        points = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(pointcloud)
+        bound_mask = np.all((points > min_corner) & (points < max_corner), axis=1)
+        points = points[bound_mask, :]
+
+        if points.shape[0] == 0:
             rospy.loginfo('No points in pointcloud')
             return
-        average = average / num_points
+
+        average = np.mean(points, axis=0)
 
         out_point = PointStamped()
-        out_point.header.frame_id = 'camera_depth_frame'
+        out_point.header.frame_id = pointcloud.header.frame_id
         # out_point.point.x = average[2]
         # out_point.point.y = -average[0]
         # out_point.point.z = -average[2]
@@ -44,7 +52,7 @@ class TargetPositionEstimator:
         out_point.point.z = average[2]
         print(out_point)
 
-        out_point = self.tf_listener.transformPoint(target_frame='odom', ps=out_point)
+        # out_point = self.tf_listener.transformPoint(target_frame='/odom', ps=out_point)
 
         self.point_pub.publish(out_point)
 
